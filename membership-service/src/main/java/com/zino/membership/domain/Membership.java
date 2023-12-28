9df350b46582b1c0405fbc8ee83fd87c3587228c
공진호 <wlsgh7608@naver.com>
2023-12-29 00:47:02 +0900

    [2023-12-29] Membership 도메인 추가

---
+++ membership-service/src/main/java/com/zino/membership/domain/Membership.java
@@ -0,0 +1,95 @@
+package com.zino.membership.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Value;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Membership {
+    @Getter
+    private final String membershipId;
+    @Getter
+    private final String name;
+    @Getter
+    private final String email;
+    @Getter
+    private final String address;
+    @Getter
+    private final boolean isValid;
+    @Getter
+    private final boolean isCorp;
+
+    public static Membership generateMember(
+            MembershipId membershipId,
+            MembershipName membershipName,
+            MembershipEmail membershipEmail,
+            MembershipAddress membershipAddress,
+            MembershipIsValid membershipIsValid,
+            MembershipIsCorp membershipIsCorp
+    ) {
+        return new Membership(
+                membershipId.idValue,
+                membershipName.nameValue,
+                membershipEmail.emailValue,
+                membershipAddress.addressValue,
+                membershipIsValid.isValidValue,
+                membershipIsCorp.isCorpValue
+        );
+    }
+
+    @Value
+    public static class MembershipId{
+
+        public MembershipId(String value) {
+            this.idValue = value;
+        }
+
+        String idValue;
+    }
+    @Value
+    public static class MembershipName{
+
+        public MembershipName(String value) {
+            this.nameValue = value;
+        }
+
+        String nameValue;
+    }
+    @Value
+    public static class MembershipEmail{
+
+        public MembershipEmail(String value) {
+            this.emailValue = value;
+        }
+
+        String emailValue;
+    }
+    @Value
+    public static class MembershipAddress{
+
+        public MembershipAddress(String value) {
+            this.addressValue = value;
+        }
+
+        String addressValue;
+    }
+    @Value
+    public static class MembershipIsValid{
+
+        public MembershipIsValid(boolean value) {
+            this.isValidValue = value;
+        }
+
+        boolean isValidValue;
+    }
+    @Value
+    public static class MembershipIsCorp{
+
+        public MembershipIsCorp(boolean value) {
+            this.isCorpValue = value;
+        }
+
+        boolean isCorpValue;
+    }
+}
