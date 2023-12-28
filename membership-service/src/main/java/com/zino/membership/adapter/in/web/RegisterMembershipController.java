1126921c1eb720acf099c873a4c92998953db9da
공진호 <wlsgh7608@naver.com>
2023-12-29 05:27:41 +0900

    [2023-12-29] 회원가입 컨트롤러 및 request dto

--- membership-service/src/main/java/com/zino/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/zino/membership/adapter/in/web/RegisterMembershipController.java
@@ -2,17 +2,17 @@ package com.zino.membership.adapter.in.web;
 
 import com.zino.membership.common.WebAdapter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/membership")
 public class RegisterMembershipController {
-    @GetMapping("/test")
-    void test() {
+    @PostMapping("/register")
+    public void register(@RequestBody RegisterMembershipRequest request) {
+
         System.out.println("Test Controller");
     }
 
---
+++ membership-service/src/main/java/com/zino/membership/adapter/in/web/RegisterMembershipRequest.java
@@ -0,0 +1,17 @@
+package com.zino.membership.adapter.in.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterMembershipRequest {
+
+    private String name;
+    private String email;
+    private String address;
+    private boolean isCorp;
+
+}
