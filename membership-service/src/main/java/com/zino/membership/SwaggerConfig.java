938bdb6e3859eae0978548168c2d70c99cd987f9
jinho <wlsgh7608@naver.com>
2024-02-16 18:59:13 +0900

    [2024-02-16] feat: Swagger 환경 설정 추가

---
+++ membership-service/src/main/java/com/zino/membership/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.zino.membership;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Membership API",
+                version = "0.0.1",
+                description = "Membership API"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+}
