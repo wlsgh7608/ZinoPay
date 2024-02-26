aade7a799cf48f5efb31e12f55d48473b7cae6bd
jinho <wlsgh7608@naver.com>
2024-02-26 17:19:31 +0900

    [2024-02-26] project init

---
+++ banking-service/src/test/java/com/zino/banking/BankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zino.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
